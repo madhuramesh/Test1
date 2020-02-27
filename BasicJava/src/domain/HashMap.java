35aae128f5ab6df4e45abc95ec8e0713a5294c89
madhumathiramesh <madhumathi.ramesh@gmail.com>
2020-02-26 22:16:20 -0800

    hi

---
+++ BasicJava/src/domain/HashMap.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class HashMap {
+	/*public static void hashMap() {
+		HashMap<Integer, ArrayList<String>> mapList = new HashMap<Integer, ArrayList<String>>();
+		ArrayList<String> arrList1 = new ArrayList<String>();
+		arrList1.add("apple");
+		arrList1.add("banana");
+		arrList1.add("orange");
+		arrList1.add("grapes");
+		mapList.put(1, arrList1);
+		
+		ArrayList<String> arrList2 = new ArrayList<String>();
+		arrList2.add("red");
+		arrList2.add("green");
+		arrList2.add("blue");
+		arrList2.add("yellow");
+		mapList.put(2, arrList2);
+		
+		//HashMap<String, Integer> hashMap = new HashMap<>();
+		
+		
+	}*/
+
+}
